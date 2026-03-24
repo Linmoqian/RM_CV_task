4dc208fbc5191aafd24cb7b39eef1c824fac0133
Linmoqian <3047076929@qq.com>
2026-03-24 22:23:21 +0800

    对象方法添加

--- CV_task/二.应用题/main.py
+++ CV_task/二.应用题/main.py
@@ -9,3 +9,27 @@ class Armor:
         Detect armor in the given image.
         """
         pass
+
+    def Central_Point():
+        """
+        Calculate the central point of the detected armor.
+        """
+        pass
+
+    def Diagonal() :
+        """
+        Calculate the diagonal of the detected armor.
+        """
+        pass
+
+    def Armor_Point():
+        """
+        Calculate the armor points of the detected armor.
+        """
+        pass
+
+    def Armor_Color():
+        """
+        Determine the color of the detected armor.
+        """
+        pass
