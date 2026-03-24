d2fb7123583b2a02c2459ca493d73eccdd49c4d7
Linmoqian <3047076929@qq.com>
2026-03-24 22:22:21 +0800

    对象定义

---
+++ CV_task/二.应用题/main.py
@@ -0,0 +1,11 @@
+import cv2
+
+class Armor:
+    def __init__(self):
+        pass
+
+    def detect(self, image, ID,color,position,width,height):
+        """
+        Detect armor in the given image.
+        """
+        pass
